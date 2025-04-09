30542c4bf372088f361e9a8c0045d74299d6f9d5
Rajeshwari259 <rajiammu293@gmail.com>
2025-04-09 16:03:53 +0530

    Initial commit with frontend and Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+#simple dockerized web app
+FROM nginx:alpine
+
+WORKDIR /usr/share/nginx/html
+
+COPY . .
+
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
+
---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('changeTextBtn').addEventListener('click', function() {
+
+    document.getElementById('dynamicText').textContent = 'You clicked the button! The text has changed.';
+});
